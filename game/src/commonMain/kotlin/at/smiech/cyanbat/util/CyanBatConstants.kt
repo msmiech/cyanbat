498f6fc74d39cacac604ddcd3f13ffb3d0e34d4f
Martin Smiech <marty.smiech@gmail.com>
2026-09-11 00:29:24 +0200

    Remove unused constants.

--- game/src/commonMain/kotlin/at/smiech/cyanbat/util/CyanBatConstants.kt
+++ game/src/commonMain/kotlin/at/smiech/cyanbat/util/CyanBatConstants.kt
@@ -1,7 +1,5 @@
 package at.smiech.cyanbat.util
 
-const val DEBUG = false
-const val TAG = "CyanBat"
 const val INITIAL_ENEMY_GENERATION_INTERVAL = 5_000L // in ms
 const val MINIMUM_ENEMY_GENERATION_INTERVAL = 1_000L // in ms
 
